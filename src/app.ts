55359c179c0e1864d81750d63ab593bed42fed25
Vaibhav Matere <vpm00718897@gmail.com>
2022-10-07 12:31:16 +0530

    final commit

--- src/app.ts
+++ src/app.ts
@@ -4,74 +4,74 @@ const app = express();
 
 app.use(express.json());
 
-// app.get('/', (req:Request, res:Response) => {
-//      // return res.send("first ejs app");
-//      // res.json({
-//      //      success: true,
-//      //      name:"Vaibhav",
-//      // })
-//      res.redirect("http://example.gom")
-//});
+app.get('/', (req:Request, res:Response) => {
+     // return res.send("first ejs app");
+     // res.json({
+     //      success: true,
+     //      name:"Vaibhav",
+     // })
+     res.redirect("http://example.gom")
+});
 
-//chain requests 
-// app.route('/api/shipments')
-//      .get((req: Request, res: Response) => {
-//           return res.send("GET all the books");
-//      })
-//      .post((req: Request, res: Response) => {
-//           return res.send("CREATE new book");
-//      })
-//      .put((req: Request, res: Response) => {
-//           return res.send("UPDATE the book");
-//      })
-//      .delete((req: Request, res: Response) => {
-//           return res.send("DELETE the book");
-//      })
-//      .all((req: Request, res: Response) => {
-//           return res.send("you made a WRONG request");
-//      })
+// chain requests 
+app.route('/api/shipments')
+     .get((req: Request, res: Response) => {
+          return res.send("GET all the books");
+     })
+     .post((req: Request, res: Response) => {
+          return res.send("CREATE new book");
+     })
+     .put((req: Request, res: Response) => {
+          return res.send("UPDATE the book");
+     })
+     .delete((req: Request, res: Response) => {
+          return res.send("DELETE the book");
+     })
+     .all((req: Request, res: Response) => {
+          return res.send("you made a WRONG request");
+     })
 
-// app.post('/api/data', (req:Request, res:Response) => {
-//      console.log(req.body);
-//      return res.sendStatus(200);
-// })
+app.post('/api/data', (req:Request, res:Response) => {
+     console.log(req.body);
+     return res.sendStatus(200);
+})
 
-// app.all('/api/all', (req:Request, res:Response) => {
-//      return res.sendStatus(200)
-// })
+app.all('/api/all', (req:Request, res:Response) => {
+     return res.sendStatus(200)
+})
 
-// app.get('/health', (req, res)=> res.sendStatus(200))
-// app.get('/ab*cd', (req, res) => res.send('/ab*cd'))
-// app.get('/abc/', (req, res) => res.send('/abc')) //regular expression
+app.get('/health', (req, res)=> res.sendStatus(200))
+app.get('/ab*cd', (req, res) => res.send('/ab*cd'))
+app.get('/abc/', (req, res) => res.send('/abc')) //regular expression
 
-// app.get('/api/books/:bookId', (req:Request, res:Response) => {
-//      console.log(req.params);
-//      return res.send(req.params.bookId);
-// })
+app.get('/api/books/:bookId', (req:Request, res:Response) => {
+     console.log(req.params);
+     return res.send(req.params.bookId);
+})
 
-// app.get('/api/books/:bookId/:authorId', (req:Request, res:Response) => {  //multiple id
-//      console.log(req.params);               
-//      return res.send(req.params);
-// })
+app.get('/api/books/:bookId/:authorId', (req:Request, res:Response) => {  //multiple id
+     console.log(req.params);               
+     return res.send(req.params);
+})
 
 // by making separate functions
 
-// function idHandler(req:Request, res:Response){
-//      console.log(req.params)             
-//      return res.send(req.params)
-// }
+function idHandler(req:Request, res:Response){
+     console.log(req.params)             
+     return res.send(req.params)
+}
 
-// app.get('/api/serial/:serilId/:episodeId', idHandler)
+app.get('/api/serial/:serilId/:episodeId', idHandler)
 
 // by making built-in "NextFunction" method
 
-// function getCoinHandler(req:Request, res:Response, next:NextFunction) {
-//      console.log(req.params);
+function getCoinHandler(req:Request, res:Response, next:NextFunction) {
+     console.log(req.params);
 
-//      return res.send(req.params);
-// }
+     return res.send(req.params);
+}
 
-// app.get('/api/:cryptoId/:tokenId/:capId', getCoinHandler)
+app.get('/api/:cryptoId/:tokenId/:capId', getCoinHandler)
 
 // two functions
 
